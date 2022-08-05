e8869390c2c3af581147a321268e7821537a773b
Felipe Ribeiro <felipeonlinebr@gmail.com>
2022-08-05 18:52:57 -0300

    Assets and css included

---
+++ src/assets/data.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    title: "Mount Fuji",
+    location: "Japan",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+  },
+  {
+    title: "Nobody's land",
+    location: "End of the world",
+    googleMapsUrl: "https://goo.gl/maps/HfDFp8pLMB4k95EE9",
+    startDate: "00 The, 0000",
+    endDate: "00 End, 0000",
+    description: "Wow, sed vere curiosus es.",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRONPJcGRroKJoAnRyBj6exe568jaJz1Rd-tA&usqp=CAU",
+  },
+  {
+    title: "Pedro Leopoldo",
+    location: "Brazil",
+    googleMapsUrl: "https://goo.gl/maps/MqtJ8qPRHxfL6zjh9",
+    startDate: "13 Jul, 2010",
+    endDate: "20 Dec, 2010",
+    description:
+      "Pedro Leopoldo is a Brazilian municipality in the state of Minas Gerais known for being the birthplace of Chico Xavier, a medium, philanthropist and one of the most important exponents of Spiritism.",
+    imageUrl:
+      "https://i0.wp.com/circuitodasgrutas.com.br/wp-content/uploads/2016/05/pedroleopoldo.jpg?fit=1200%2C801&ssl=1",
+  },
+];
---
+++ src/assets/images.js
@@ -0,0 +1,17 @@
+export default [
+  "https://source.unsplash.com/WLxQvbMyfas",
+  "https://source.unsplash.com/JmuyB_LibRo",
+  "https://source.unsplash.com/3PeSjpLVtLg",
+];
+
+// Photos from Unsplash
+// (can use these URLs as your img src)
+
+// Japan
+// https://source.unsplash.com/WLxQvbMyfas
+
+// Australia
+// https://source.unsplash.com/JmuyB_LibRo
+
+// Norway
+// https://source.unsplash.com/3PeSjpLVtLg
